9ed1ef159e73202f71d53631389b16d824c53f9f
Gregstr <gregstr@gregstr.eu>
2025-11-05 15:50:52 +0100

    Added a basic window and renderer creation logic, cmake finds SDL2 from path, needs SDL2 installed on system and added to path

--- main.c
+++ main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <time.h>
+
+#include <SDL2/SDL.h>
 
 int main()
 {
 	printf("Goodbye, world!\n");
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 800, SDL_WINDOW_SHOWN);
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	SDL_bool quit = SDL_FALSE;
+
+	while (!quit) {
+		SDL_Event e;
+		SDL_PollEvent(&e);
+		if (e.type == SDL_QUIT)
+			quit = SDL_TRUE;
+
+		SDL_RenderPresent(renderer);
+
+
+	}
+
+	SDL_DestroyRenderer(renderer);
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
 
 	return 0;
 }

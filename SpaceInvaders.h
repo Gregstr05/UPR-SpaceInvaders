0df0bd341db8e4294b35d3dc3e27989b8a2e3e29
Gregstr <gregstr@gregstr.eu>
2025-11-09 13:48:17 +0100

    Defined more data structs

--- SpaceInvaders.h
+++ SpaceInvaders.h
@@ -9,12 +9,42 @@ typedef struct {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_bool bShouldClose;
-}GameState;
+} GameState;
 
+typedef struct {
+    int posX;
+    int posY;
+    int Lifes;
+} Player;
+
+typedef struct {
+    Player *player;
+
+} GameData;
+
+/**
+ * Initializes SDL and stores important pointers in the GameState reference
+ * @param state GameState reference
+ */
 void Init(GameState *state);
+
+/**
+ * Cleans up SDL
+ * @param state GameState reference
+ */
 void Destroy(GameState *state);
 
+/**
+ * Gets called every frame and updates game values
+ * @param deltaTime Time difference between this and previous frame
+ * @param state GameState reference
+ */
 void Update(double deltaTime, GameState *state);
+
+/**
+ * Gets called every frame after Update() and renders based on game data
+ * @param state GameState reference
+ */
 void Render(GameState *state);
 
 #endif //SPACEINVADERS_SPACEINVADERS_H
